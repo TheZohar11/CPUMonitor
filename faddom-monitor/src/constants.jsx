4c393912fde75cc27b465cfd06044032af1f71e9
zohar <zoharsim2000@gmail.com>
2026-02-07 18:31:54 +0200

    adding stylings to DropDown and Button

--- faddom-monitor/src/constants.jsx
+++ faddom-monitor/src/constants.jsx
@@ -1,5 +1,4 @@
 export const INTERVAL_OPTIONS = [
-  { label: "1 Minute", value: 60 },
   { label: "5 Minutes", value: 300 },
   { label: "15 Minutes", value: 900 },
   { label: "1 Hour", value: 3600 },
