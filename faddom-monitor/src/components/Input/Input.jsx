f8121a714c6bcc9ec984ece59305440929d4e737
zohar <zoharsim2000@gmail.com>
2026-02-07 22:56:16 +0200

    format with prettier

--- faddom-monitor/src/components/Input/Input.jsx
+++ faddom-monitor/src/components/Input/Input.jsx
@@ -5,7 +5,13 @@ export default function Input({ text, placeholder, type, value, onChange }) {
   return (
     <div className="input-container">
       <h3>{text}</h3>
-      <input className="input-field" type={type} placeholder={placeholder} value={value} onChange={onChange} />
+      <input
+        className="input-field"
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+      />
     </div>
   );
 }
